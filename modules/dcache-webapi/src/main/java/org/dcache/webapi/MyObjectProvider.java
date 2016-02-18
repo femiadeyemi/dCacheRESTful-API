a0266d786f1aecbe1a527b786ef57c8b3c11b6a5
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2016-02-18 15:32:58 +0100

    I applied Marina restApi patch to the curent master

---
+++ modules/dcache-webapi/src/main/java/org/dcache/webapi/MyObjectProvider.java
@@ -0,0 +1,74 @@
+package org.dcache.webapi;
+
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+
+
+/**
+ * Used to support common Media Type Representations
+ *
+ */
+
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class MyObjectProvider implements ContextResolver<ObjectMapper> {
+
+
+    final ObjectMapper defaultObjectMapper;
+    final ObjectMapper combinedObjectMapper;
+
+    public MyObjectProvider() {
+        defaultObjectMapper = createDefaultMapper();
+        combinedObjectMapper = createCombinedObjectMapper();
+    }
+
+    private static ObjectMapper createCombinedObjectMapper() {
+        return new ObjectMapper()
+                .configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false)
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, true)
+                .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
+
+                .setAnnotationIntrospector(createJaxbJacksonAnnotationIntrospector());
+    }
+
+    private static ObjectMapper createDefaultMapper() {
+        final ObjectMapper result = new ObjectMapper();
+        result.enable(SerializationFeature.INDENT_OUTPUT);
+
+        return result;
+    }
+
+    private static AnnotationIntrospector createJaxbJacksonAnnotationIntrospector() {
+
+        final AnnotationIntrospector jaxbIntrospector = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+        final AnnotationIntrospector jacksonIntrospector = new JacksonAnnotationIntrospector();
+
+        return AnnotationIntrospector.pair(jacksonIntrospector, jaxbIntrospector);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+
+        System.out.println("MyObjectProvider.getContext() called with type: " + type);
+        if (type == java.util.ArrayList.class) {
+            return combinedObjectMapper;
+        } else {
+            return defaultObjectMapper;
+        }
+    }
+}
